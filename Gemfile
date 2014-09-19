3839cde6a3d5ac111a5073218792728f67be1a77
Horacio Bertorello <syrii@msn.com>
2014-09-19 16:10:08 -0300

    Deploy with rsync via middleman-deploy.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 gem "middleman", "~> 3.3.5"
 gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-deploy", "~> 0.3.0"
--- config.rb
+++ config.rb
@@ -8,3 +8,9 @@ set :images_dir, 'images'
 
 activate :asset_hash
 activate :directory_indexes
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host = 'svankmajer.net'
+  deploy.path = '/opt/svankmajer.net'
+end
