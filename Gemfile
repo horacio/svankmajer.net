22cfc3ef3d15b8488ecd9bd03c28bf9b45026006
Horacio A. Bertorello <horacio.bertorello@gmail.com>
2016-09-30 12:23:21 -0300

    Things.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 3.3.5'
 gem 'middleman-livereload', '~> 3.1.0'
+gem 'middleman-deploy', '~> 1.0'
+
+group :development do
+  gem 'pry'
+  gem 'pry-nav'
+end
--- config.rb
+++ config.rb
@@ -1,3 +1,10 @@
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host = 'svankmajer.net'
+  deploy.path = '/var/www/svankmajer.net'
+  deploy.user = 'horacio'
+end
+
 configure :development do
   activate :livereload
 end
